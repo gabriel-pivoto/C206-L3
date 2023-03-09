3ef177df02e0383f1e93556b9c25ad12038a8833
Gabriel Pivoto <gabrielspivoto@gmail.com>
2023-03-08 23:01:39 -0300

    enviando exercicio1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
-public class Main {
+import java.util.Scanner;
+
+public class Main
+{
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner (System.in);
+        int op;
+        System.out.println("Entrar com o valor: ");
+        op = sc.nextInt();
+
+        if(op < 18.5){
+            System.out.println("abaixo do peso");
+        }else if(op <= 24.9){
+            System.out.println("Peso ideal (parabéns)");
+        }else if(op <= 29.9){
+            System.out.println("levemente acima do peso");
+        }else if(op <= 34.9){
+            System.out.println("Obesidade grau I");
+        }else if(op <= 39.9){
+            System.out.println("Obesidade grau II (severa)");
+        }else{
+            System.out.println("Obesidade III (mórbida)");
+        }
     }
 }
